c0ea2542a8115fa74011d2c721b53e78dd8d096d
Kaitozerob <u202223781@upc.edu.pe>
2025-05-28 23:53:22 -0500

    feat(ui): add UserInputValidator to check user input validity

--- src/main/java/ui/UserInputValidator.java
+++ src/main/java/ui/UserInputValidator.java
@@ -1,4 +1,12 @@
 package main.java.ui;
 
 public class UserInputValidator {
+    public static boolean isValidAmount(String input) {
+        try {
+            Double.parseDouble(input);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
 }
